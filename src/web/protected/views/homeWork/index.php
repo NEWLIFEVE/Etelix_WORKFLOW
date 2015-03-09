f67105e09f0eec971d2d0cdb3b710efa5ab0538d
Israel Tarazona <israeltarazona@gmail.com>
2015-03-09 10:17:18 -0430

    nuevas rutas para los link de las aplicaciones

--- src/web/protected/views/homeWork/index.php
+++ src/web/protected/views/homeWork/index.php
@@ -3,10 +3,10 @@
         <div class="contenidos_columnas">
             
              <div class="secundario form-group">
-                 <a href="http://sori.sacet.com.ve/" target="_blank"><img  src="/themes/metronic/admin/pages/img/icono_ribela-01.png" class="zoom" style="width: 26%; margin-left: 30% " /></a> 
+                 <a href="http://sori.ribela.net/" target="_blank"><img  src="/themes/metronic/admin/pages/img/icono_ribela-01.png" class="zoom" style="width: 26%; margin-left: 30% " /></a> 
             </div>
             <div class="secundario form-group">
-                <a href="http://renoc.sacet.com.ve/" target="_blank"><img  src="/themes/metronic/admin/pages/img/icono_ribela-02.png" class="zoom" style="width: 25%; margin-left: 13%"/></a> 
+                <a href="http://renoc.ribela.net/" target="_blank"><img  src="/themes/metronic/admin/pages/img/icono_ribela-02.png" class="zoom" style="width: 25%; margin-left: 13%"/></a> 
             </div>
             <div  class="secundarioderecha form-group">
                 <a href="#" ><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 31%; margin-left: -9%"/></a>
@@ -20,10 +20,10 @@
         </div>
         <div class="contenidos_columnas">
             <div class="secundario form-group">
-               <a href="http://sinca.sacet.com.ve/" target="_blank"><img src="/themes/metronic/admin/pages/img/icono_ribela-03.png" class="zoom" style="width: 25%; margin-left: 30% "/></a>
+               <a href="http://sinca.ribela.net/" target="_blank"><img src="/themes/metronic/admin/pages/img/icono_ribela-03.png" class="zoom" style="width: 25%; margin-left: 30% "/></a>
             </div>
             <div class="secundario form-group">
-                <a href="http://sine.sacet.com.ve/" target="_blank"><img src="/themes/metronic/admin/pages/img/icono_ribela-04.png" class="zoom" style="width: 25%; margin-left: 13%"/></a> 
+                <a href="http://sine.ribela.net/" target="_blank"><img src="/themes/metronic/admin/pages/img/icono_ribela-04.png" class="zoom" style="width: 25%; margin-left: 13%"/></a> 
             </div>
             <div class="secundarioderecha form-group">
                  <a href="#" ><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 31%; margin-left: -9%"/></a> 
@@ -36,13 +36,13 @@
         </div>
         <div class="contenidos_columnas">
             <div class="secundario form-group">
-                   <a href="http://aru.etelix.com/" target="_blank"><img src="/themes/metronic/admin/pages/img/icono_ribela-05.png" class="zoom" alt="" style="width: 25%; margin-left: 30% " /></a>                         
+                   <a href="http://aru.ribela.net/" target="_blank"><img src="/themes/metronic/admin/pages/img/icono_ribela-05.png" class="zoom" alt="" style="width: 25%; margin-left: 30% " /></a>                         
             </div>
             <div class="secundario form-group">
-                  <a href="http://pig.etelix.com/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-07.png" class="zoom" alt="" style="width: 25%; margin-left: 13%" /></a>
+                  <a href="http://pig.ribela.net/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-07.png" class="zoom" alt="" style="width: 25%; margin-left: 13%" /></a>
             </div>
             <div class="secundarioderecha form-group">
-                <a href="http://etelixtv.sacet.com.ve/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-08.png" class="zoom" alt="" style="width: 33%; margin-left: -9%"/></a> 
+                <a href="http://etelixtv.ribela.net/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-08.png" class="zoom" alt="" style="width: 33%; margin-left: -9%"/></a> 
             </div>
             <div class="secundario form-group">
                 <a href="#"><img id ="load_photo" src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" alt="" style="width: 48%; margin-left: -9%"/></a>
@@ -51,13 +51,13 @@
         </div>
         <div class="contenidos_columnas">
             <div class="secundario form-group">
-                     <a href="http://etts.etelix.com/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-06.png" class="zoom" alt="" style="width: 25%; margin-left: 30% "/></a> 
+                     <a href="http://etts.ribela.net/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-06.png" class="zoom" alt="" style="width: 25%; margin-left: 30% "/></a> 
             </div>
             <div class="secundario form-group">
-                    <a href="http://atc.etelix.com/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-09.png" class="zoom" alt="" style="width: 25%; margin-left: 13%"/></a> 
+                    <a href="http://atc.ribela.net/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-09.png" class="zoom" alt="" style="width: 25%; margin-left: 13%"/></a> 
             </div>
             <div class="secundarioderecha form-group">
-                <a href="http://cctt.etelixcontactcenter.com/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" alt="" style="width: 33%; margin-left: -9%"/></a>
+                <a href="#" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" alt="" style="width: 33%; margin-left: -9%"/></a>
             </div>
             <div class="secundario form-group">
                 <a href="#"><img id ="load_photo" src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" alt="" style="width: 48%; margin-left: -9%"/></a>
