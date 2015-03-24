dd7ef42eb6f41699a274cdcfc28f53def8ee9530
root <root@server01.etelixgroup.com>
2015-03-24 17:54:28 -0430

    ..

--- src/web/protected/config/db.php
+++ src/web/protected/config/db.php
@@ -3,13 +3,9 @@
 return array(
 	'components'=>array(
 		'db'=>array(
-			'connectionString'=>'pgsql:host=127.0.0.1;port=5432;dbname=dev_aru_25092014',
-			'emulatePrepare'=>true,
+			'connectionString'=>'pgsql:host=67.215.160.228;port=5432;dbname=aru',
 			'username'=>'postgres',
-            'password'=>'123',
-			'charset'=>'utf8',
-			'enableProfiling'=>true,
-        	'enableParamLogging'=>true,
+            'password'=>'rkCQ16DjK3nwVDR6',
             ),
 		)
 	);
