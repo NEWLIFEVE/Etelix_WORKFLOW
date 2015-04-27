79c5f259b06d956beea0bd1d6780ae7c0a299d8b
Israel Tarazona <israeltarazona@gmail.com>
2015-04-27 12:59:13 -0430

    control de calidad

--- src/web/index.php
+++ src/web/index.php
@@ -1,14 +1,14 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../yii/yii.php';
+//$yii=dirname(__FILE__).'/../../../yii/yii.php';
 
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
+$yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
 require_once($yii);
 
 
