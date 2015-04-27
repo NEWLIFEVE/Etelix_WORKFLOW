16f2588d8cb008b0c8957112562bf2965df37843
David <davidl@sacet.biz>
2015-04-27 16:51:30 -0430

    Big fixed with hand cursor in links with modals

--- src/web/protected/views/homeWork/index.php
+++ src/web/protected/views/homeWork/index.php
@@ -3,30 +3,30 @@
         <div class="contenidos_columnas">
             
              <div class="secundario form-group">
-                 <a data-toggle="modal" data-target="#sori_options"><img  src="/themes/metronic/admin/pages/img/icono_ribela-01.png" class="zoom" style="width: 26%; margin-left: 30% " /></a> 
+                 <a href="#" data-toggle="modal" data-target="#sori_options"><img  src="/themes/metronic/admin/pages/img/icono_ribela-01.png" class="zoom" style="width: 26%; margin-left: 30% " /></a> 
             </div>
             <div class="secundario form-group">
                 <a href="http://renoc.ribela.net/" target="_blank"><img  src="/themes/metronic/admin/pages/img/icono_ribela-02.png" class="zoom" style="width: 25%; margin-left: 13%"/></a> 
             </div>
             <div  class="secundarioderecha form-group">
-                <a href="#" ><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 31%; margin-left: -9%"/></a>
+                <a href="#"><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 31%; margin-left: -9%"/></a>
                  <label class="control-label letra_empleado" style="margin-left: -4%"></label>
             </div>
            
             <div class="secundario form-group">
-                <a href="#" ><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 48%; margin-left: -9%"/></a>
+                <a href="#"><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 48%; margin-left: -9%"/></a>
                  <label class="control-label letra_empleado" style="margin-left: 12%"></label>
             </div>
         </div>
         <div class="contenidos_columnas">
             <div class="secundario form-group">
-               <a data-toggle="modal" data-target="#sinca_options"><img src="/themes/metronic/admin/pages/img/icono_ribela-03.png" class="zoom" style="width: 25%; margin-left: 30% "/></a>
+                <a href="#" data-toggle="modal" data-target="#sinca_options"><img src="/themes/metronic/admin/pages/img/icono_ribela-03.png" class="zoom" style="width: 25%; margin-left: 30% "/></a>
             </div>
             <div class="secundario form-group">
-                <a data-toggle="modal" data-target="#sine_options"><img src="/themes/metronic/admin/pages/img/icono_ribela-04.png" class="zoom" style="width: 25%; margin-left: 13%"/></a> 
+                <a href="#" data-toggle="modal" data-target="#sine_options"><img src="/themes/metronic/admin/pages/img/icono_ribela-04.png" class="zoom" style="width: 25%; margin-left: 13%"/></a> 
             </div>
             <div class="secundarioderecha form-group">
-                 <a href="#" ><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 31%; margin-left: -9%"/></a> 
+                 <a href="#"><img src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" style="width: 31%; margin-left: -9%"/></a> 
                  <label class="control-label letra_empleado" style="margin-left: 1%"></label>
             </div>
             <div class="secundario form-group">
@@ -51,10 +51,10 @@
         </div>
         <div class="contenidos_columnas">
             <div class="secundario form-group">
-                     <a href="http://etts.ribela.net/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-06.png" class="zoom" alt="" style="width: 25%; margin-left: 30% "/></a> 
+              <a href="http://etts.ribela.net/" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-06.png" class="zoom" alt="" style="width: 25%; margin-left: 30% "/></a> 
             </div>
             <div class="secundario form-group">
-                    <a data-toggle="modal" data-target="#atc_options"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-09.png" class="zoom" alt="" style="width: 25%; margin-left: 13%"/></a> 
+              <a href="#" data-toggle="modal" data-target="#atc_options"><img id ="load_photo" src="/themes/metronic/admin/pages/img/icono_ribela-09.png" class="zoom" alt="" style="width: 25%; margin-left: 13%"/></a> 
             </div>
             <div class="secundarioderecha form-group">
                 <a href="#" target="_blank"><img id ="load_photo" src="/themes/metronic/admin/pages/img/ribela-soon.png" class="zoom" alt="" style="width: 33%; margin-left: -9%"/></a>
