b98cd9c9b18f6d9cd896c5e816a5e8dfa210a4a7
Alexis Rivas <sixela.savir@gmail.com>
2015-05-13 18:01:25 -0430

    logueo desde ribela, aru y pig

--- src/web/index.php
+++ src/web/index.php
@@ -1,8 +1,32 @@
 <?php
+define('SERVER_NAME_PROD', 'ribela.net');
+define('SERVER_NAME_LOCAL', 'ribela.local');
+
+
+
+
+$server=$_SERVER['SERVER_NAME'];
+
+switch ($server){
+    case SERVER_NAME_PROD:
+            define('SERVER_NAME_RIBELA', 'ribela.net');
+            define('SERVER_NAME_ARU', 'aru.ribela.net');
+            define('SERVER_NAME_PIG', 'pig.ribela.net');
+            $yii=dirname(__FILE__).'/../../../yii/yii.php';
+        break;
+    default:
+            define('SERVER_NAME_RIBELA', 'ribela.local');
+            define('SERVER_NAME_ARU', 'aru.local');
+            define('SERVER_NAME_PIG', 'pig.local');
+            $yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
+            $gii=require(dirname(__FILE__).'/protected/config/gii.php');
+        break;
+}
+
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../yii/yii.php';
-//$yii=dirname(__FILE__).'/../../yii/framework/yii.php';
+//$yii=dirname(__FILE__).'/../../../yii/yii.php';
+
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
@@ -14,7 +38,7 @@ require_once($yii);
 
 $main=require(dirname(__FILE__).'/protected/config/main.php');
 $db=require(dirname(__FILE__).'/protected/config/db.php');
-$gii=require(dirname(__FILE__).'/protected/config/gii.php');
+
 
 $config=CMap::mergeArray($main,$db,$gii);
 
